51ee4dc93d26f4447edf9c4fead42471bd3cfa75
AhmadMahmoudTarfaoui <ahmadmahmoudtarfaoui@outlook.com>
2025-03-14 14:51:03 +0000

    refactor layout structure and scaling for responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,26 @@ import Form from "./components/Form";
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center h-screen bg-slate-50">
-      <motion.div
-        initial={{ borderBottom: "2px solid #6d28d9" }}
-        animate={{
-          borderBottom: "2px solid #f8fafc",
-          transition: { delay: 2 },
-        }}
-        className="z-10 w-[600px] h-[250px] bg-slate-50 flex place-items-end justify-center overflow-hidden pb-4 mb-6"
-      >
-        <motion.p
-          initial={{ y: 65 }}
-          animate={{ y: 0, transition: { type: "spring", duration: 4 } }}
-          className="text-4xl font-semibold text-violet-700"
+    <div className="h-screen flex justify-center bg-slate-50">
+      <div className="scale-[.60] md:scale-75 lg:scale-100 flex flex-col items-center m-auto">
+        <motion.div
+          initial={{ borderBottom: "2px solid #6d28d9" }}
+          animate={{
+            borderBottom: "2px solid #f8fafc",
+            transition: { delay: 2 },
+          }}
+          className="z-10 -mt-[70vh] md:-mt-[50vh] lg:-mt-[200px] w-[600px] h-[70vh] md: lg:h-[250px] bg-slate-50 flex place-items-end justify-center overflow-hidden pb-4 mb-6"
         >
-          Multi-Step Form
-        </motion.p>
-      </motion.div>
-      <Form />
+          <motion.p
+            initial={{ y: 65 }}
+            animate={{ y: 0, transition: { type: "spring", duration: 4 } }}
+            className="text-4xl font-semibold text-violet-700"
+          >
+            Multi-Step Form
+          </motion.p>
+        </motion.div>
+        <Form />
+      </div>
     </div>
   );
 };
